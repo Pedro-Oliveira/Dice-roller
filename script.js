2318ffaea23812f1a901161c1ec65df231f6987c
Pedro Henrique Pinto O. Santos <pdhp151001@gmail.com>
2022-03-06 18:31:12 -0300

    modifier's-script

--- script.js
+++ script.js
@@ -1,10 +1,18 @@
-let dice = document.querySelectorAll("img")
+let myinput = document.getElementById("my-input")
+
+function stepper(btn){
+    let id = btn.getAttribute("id");
+    let val = myinput.getAttribute("value");
+    let total = (id == "diminuir") ? (parseInt(val)-1): (parseInt(val)+1);
+    myinput.setAttribute("value", total);
+}
 function rolar(){
     let rolagem = Math.floor(Math.random() * 20 +1);
+    let valor = myinput.getAttribute("value");
 
     spin()
     document.getElementById("result").innerHTML = rolagem;
-    document.getElementById("span").innerHTML = rolagem;
+    document.getElementById("span").innerHTML = parseInt(valor)+rolagem;
     if (rolagem === 20){
         document.getElementById("result").style.color = "var(--green)";
         document.getElementById("body").style.backgroundColor = "var(--dark-green)";
